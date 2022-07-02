5707944367e0bb56957a556a4440fba6575f12d0
Guilherme Machado <gmachado@sertrading.lan>
2022-07-02 01:30:53 -0300

    add: new tests error qhen provided is not a number

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -7,3 +7,25 @@ it('should sum 2 and 2 and the result must be 4', () => {
 it('should sum 2  and 2 even one of them is a string and the result must be 4 ', () => {
     expect(sum('2', '2')).toBe(4);
 });
+
+it('should throw an error if what is provided to the method cannot be numeric', () => {
+    expect(() => {
+        sum('', '2')
+    }).toThrowError();
+
+    expect(() => {
+        sum([2.2])
+    }).toThrowError();
+
+    expect(() => {
+        sum({})
+    }).toThrowError();
+
+    expect(() => {
+        sum({ "numeric": 2, "numeric2": 2 })
+    }).toThrowError();
+
+    expect(() => {
+        sum()
+    }).toThrowError();
+});
