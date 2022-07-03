29e9ee6c3635ad4349a4bafc4837570ba697950c
Guilherme Machado <gmachado@sertrading.lan>
2022-07-03 13:06:30 -0300

    add: added new test when case is failed

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,17 +1,32 @@
-const { queryString } = require('./queryString')
+const { queryString } = require("./queryString");
 
+describe("Object to query String", () => {
+  it(" should create  a valid query string when an object is  provided", () => {
+    const object = {
+      name: "Guilherme",
+      profession: "SoftwareEngineer",
+    };
+    expect(queryString(object)).toBe(
+      "name=Guilherme&profession=SoftwareEngineer",
+    );
+  });
+  it("should create a valid  query even when an array is passed as value ", () => {
+    const object = {
+      name: "Guilherme",
+      abilities: ["JS", "TDD"],
+    };
+    expect(queryString(object)).toBe("name=Guilherme&abilities=JS,TDD");
+  });
 
-describe('Object to query String', () => {
-    it(' should create  a valid query string when an object is  provided', () => {
-        
-        const object = {
-            name: 'Guilherme',
-            profession: "SoftwareEngineer"
-        }
-        expect(queryString(object)).toBe('name=Guilherme&profession=SoftwareEngineer');
-    });
+  it("should throw an error when an object is passed as value", () => {
+    const object = {
+      name: "Guilherme",
+      abilities: { first: "JS", second: "TDD" },
+    };
+    expect(() => {
+      queryString(object);
+    }).toThrowError();
+  });
 });
 
-describe('Query string to object', () => {
-    
-});
+describe("Query string to object", () => {});
