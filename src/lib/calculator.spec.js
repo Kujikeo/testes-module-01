906c5a4a374b0b2cb921de32316f660458bfe3fc
Guilherme Machado <gmachado@sertrading.lan>
2022-07-08 00:41:02 -0300

    fix: import and babel

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,31 +1,31 @@
-const { sum } = require('./calculator')
+import { sum } from "./calculator";
 
-it('should sum 2 and 2 and the result must be 4', () => {
-    //expect tobe false but received true
-    expect(sum(2, 2)).toBe(4);
-})
-it('should sum 2  and 2 even one of them is a string and the result must be 4 ', () => {
-    expect(sum('2', '2')).toBe(4);
+it("should sum 2 and 2 and the result must be 4", () => {
+  //expect tobe false but received true
+  expect(sum(2, 2)).toBe(4);
+});
+it("should sum 2  and 2 even one of them is a string and the result must be 4 ", () => {
+  expect(sum("2", "2")).toBe(4);
 });
 
-it('should throw an error if what is provided to the method cannot be numeric', () => {
-    expect(() => {
-        sum('', '2')
-    }).toThrowError();
+it("should throw an error if what is provided to the method cannot be numeric", () => {
+  expect(() => {
+    sum("", "2");
+  }).toThrowError();
 
-    expect(() => {
-        sum([2.2])
-    }).toThrowError();
+  expect(() => {
+    sum([2.2]);
+  }).toThrowError();
 
-    expect(() => {
-        sum({})
-    }).toThrowError();
+  expect(() => {
+    sum({});
+  }).toThrowError();
 
-    expect(() => {
-        sum({ "numeric": 2, "numeric2": 2 })
-    }).toThrowError();
+  expect(() => {
+    sum({ numeric: 2, numeric2: 2 });
+  }).toThrowError();
 
-    expect(() => {
-        sum()
-    }).toThrowError();
+  expect(() => {
+    sum();
+  }).toThrowError();
 });
--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -5,11 +5,12 @@ const keyValueToString = ([key, value]) => {
   return `${key}=${value}`;
 };
 
-module.exports.queryString = obj =>
-  Object.entries(obj).map(keyValueToString).join("&");
+export function queryString(obj) {
+  return Object.entries(obj).map(keyValueToString).join("&");
+}
 
-module.exports.parse = qs =>
-  Object.fromEntries(
+export function parse(qs) {
+  return Object.fromEntries(
     qs.split("&").map(item => {
       let [key, value] = item.split("=");
       if (value.indexOf(",") > -1) {
@@ -18,3 +19,4 @@ module.exports.parse = qs =>
       return [key, value];
     }),
   );
+}
